5149a767c3ceadefe94d3144f7fc21c8efe63b16
rintaro-s@24239 <120776920+rintaro-s@users.noreply.github.com>
2024-07-31 18:47:16 +0900

    Update 画像の名前を1からじゅんに.ps1

--- 画像の名前を1からじゅんに.ps1
+++ 画像の名前を1からじゅんに.ps1
@@ -1,4 +1,4 @@
-﻿Set-Location C:\Users\s-rin\Downloads\VTraining-Client-VerA02B01\train\2
+﻿Set-Location ./images
 Get-ChildItem -Filter "*.jpg"
 $i = 1
 Get-ChildItem -Filter "*.jpg" | ForEach-Object {
