fb3f8354b2655f1587eaea0decc5decc2d380df0
rintaro-s@24239 <120776920+rintaro-s@users.noreply.github.com>
2024-07-31 18:46:54 +0900

    Add files via upload

---
+++ 画像の名前を1からじゅんに.ps1
@@ -0,0 +1,7 @@
+﻿Set-Location C:\Users\s-rin\Downloads\VTraining-Client-VerA02B01\train\2
+Get-ChildItem -Filter "*.jpg"
+$i = 1
+Get-ChildItem -Filter "*.jpg" | ForEach-Object {
+    Rename-Item -Path $_.FullName -NewName "$i.jpg"
+    $i++
+}
